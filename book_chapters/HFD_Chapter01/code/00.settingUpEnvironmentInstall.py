959b069a0e3a1437ddb4a6c3abbc308b644fe9ee
Paulo H. Leocadio <22998694+paulohl@users.noreply.github.com>
2024-12-26 13:17:56 -0500

    Create 00.settingUpEnvironmentInstall.py

---
+++ book_chapters/HFD_Chapter01/code/00.settingUpEnvironmentInstall.py
@@ -0,0 +1,17 @@
+# You can install PyTorch or TensorFlow along with the Hugging Face Transformers library using pip:
+bash
+pip install transformers torch tensorflow
+
+
+# •	Additional Libraries: numpy for numerical operations
+pip install numpy
+
+# To create a virtual environment using venv, run the following commands:
+python -m venv hf-env        # Create a virtual environment
+source hf-env/bin/activate   # Activate the environment on Unix/Mac
+
+# For Windows, the command to activate the environment is slightly different:
+hf-env\Scripts\activate
+
+# when finished:
+Deactivate
